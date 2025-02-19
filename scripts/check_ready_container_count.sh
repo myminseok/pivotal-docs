6b2c15fca617a6e75ebc185767cbfa14028f019c
Minseok Kim <myminseok@users.noreply.github.com>
2025-02-19 15:19:15 +0900

    Update check_ready_container_count.sh

--- scripts/check_ready_container_count.sh
+++ scripts/check_ready_container_count.sh
@@ -1,5 +1,12 @@
-#!/bin/bash
+## check container readiness. some container is running state even if the container is not ready.
+##     state     since                  cpu    memory         disk           logging        details
+## #0   running   2025-02-19T06:12:30Z   0.3%   232.3M of 1G   224.8M of 1G   0/s of 16K/s
+## #1   running   2025-02-19T06:12:30Z   0.3%   219.3M of 1G   224.8M of 1G   0/s of 16K/s
+## #2   running   2025-02-19T06:17:06Z   0.0%   0 of 0         0 of 0         0/s of 0/s     insufficient resources: memory
+##
 
+
+#!/bin/bash
 
 app_name='spring-music'
 desired_count=3
