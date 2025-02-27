ed359fedc1b7071a5bdaf395b5c53a239fb53f66
Minseok Kim <myminseok@users.noreply.github.com>
2025-02-27 23:29:18 +0900

    Update remove-dns-aliases.sh

--- offline/custom-dns-record-with-dns-aliases/remove-dns-aliases.sh
+++ offline/custom-dns-record-with-dns-aliases/remove-dns-aliases.sh
@@ -2,3 +2,5 @@
 export TAS='cf-f30f16d0a030d67be63a'
 bosh -d $TAS ssh clock_global -c 'sudo rm -rf /var/vcap/jobs/my-aliases/dns/; sudo /var/vcap/bosh/bin/monit restart bosh-dns'
 bosh -d $TAS ssh diego_cell -c 'sudo rm -rf /var/vcap/jobs/my-aliases/dns/; sudo /var/vcap/bosh/bin/monit restart bosh-dns'
+bosh -d $TAS ssh cloud_controller -c 'sudo rm -rf /var/vcap/jobs/my-aliases/dns/; sudo /var/vcap/bosh/bin/monit restart bosh-dns'
+bosh -d $TAS ssh cloud_controller_worker -c 'sudo rm -rf /var/vcap/jobs/my-aliases/dns/; sudo /var/vcap/bosh/bin/monit restart bosh-dns'
