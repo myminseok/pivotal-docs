4501ed6184b4cba3029d1d3638302f1a6d75a6d2
Minseok Kim <myminseok@users.noreply.github.com>
2025-02-27 18:04:31 +0900

    Update apply-dns-aliases.sh

--- offline/custom-dns-record-with-dns-aliases/apply-dns-aliases.sh
+++ offline/custom-dns-record-with-dns-aliases/apply-dns-aliases.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
+export TAS='cf-f30f16d0a030d67be63a'
+bosh -d $TAS ssh clock_global -c 'sudo rm -rf /tmp/aliases.json'
+bosh -d $TAS scp ./aliases.json clock_global:/tmp/aliases.json
+bosh -d $TAS ssh clock_global -c 'sudo mkdir -p /var/vcap/jobs/my-aliases/dns/; sudo cp /tmp/aliases.json /var/vcap/jobs/my-aliases/dns/; sudo /var/vcap/bosh/bin/monit restart bosh-dns'
 
-bosh -d cf-f30f16d0a030d67be63a ssh clock_global -c 'sudo rm -rf /tmp/aliases.json'
-bosh -d cf-f30f16d0a030d67be63a scp ./aliases.json clock_global:/tmp/aliases.json
-bosh -d cf-f30f16d0a030d67be63a ssh clock_global -c 'sudo mkdir -p /var/vcap/jobs/my-aliases/dns/; sudo cp /tmp/aliases.json /var/vcap/jobs/my-aliases/dns/; sudo /var/vcap/bosh/bin/monit restart bosh-dns'
-
-bosh -d cf-f30f16d0a030d67be63a ssh diego_cell -c 'sudo rm -rf /tmp/aliases.json'
-bosh -d cf-f30f16d0a030d67be63a scp ./aliases.json diego_cell:/tmp/aliases.json
-bosh -d cf-f30f16d0a030d67be63a ssh diego_cell -c 'sudo mkdir -p /var/vcap/jobs/my-aliases/dns/; sudo cp /tmp/aliases.json /var/vcap/jobs/my-aliases/dns/; sudo /var/vcap/bosh/bin/monit restart bosh-dns'
+bosh -d $TAS ssh diego_cell -c 'sudo rm -rf /tmp/aliases.json'
+bosh -d $TAS scp ./aliases.json diego_cell:/tmp/aliases.json
+bosh -d $TAS ssh diego_cell -c 'sudo mkdir -p /var/vcap/jobs/my-aliases/dns/; sudo cp /tmp/aliases.json /var/vcap/jobs/my-aliases/dns/; sudo /var/vcap/bosh/bin/monit restart bosh-dns'
