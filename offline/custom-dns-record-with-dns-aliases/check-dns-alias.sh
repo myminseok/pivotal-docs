2c86385082106d8ab2b579b72bdccbe3193fbedd
Minseok Kim <myminseok@users.noreply.github.com>
2025-02-27 18:05:12 +0900

    Update check-dns-alias.sh

--- offline/custom-dns-record-with-dns-aliases/check-dns-alias.sh
+++ offline/custom-dns-record-with-dns-aliases/check-dns-alias.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-
-bosh -d cf-f30f16d0a030d67be63a ssh clock_global -c 'dig api.sys.lab.pcfdemo.net'
-bosh -d cf-f30f16d0a030d67be63a ssh diego_cell -c 'dig api.sys.lab.pcfdemo.net'
+export TAS='cf-f30f16d0a030d67be63a'
+bosh -d $TAS ssh clock_global -c 'dig api.sys.lab.pcfdemo.net'
+bosh -d $TAS ssh diego_cell -c 'dig api.sys.lab.pcfdemo.net'
