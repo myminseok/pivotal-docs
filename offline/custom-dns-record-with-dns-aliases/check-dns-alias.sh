c6739a29222921a2c66ae22196e8bbd11d36f440
Minseok Kim <myminseok@users.noreply.github.com>
2025-02-28 20:03:13 +0900

    Update check-dns-alias.sh

--- offline/custom-dns-record-with-dns-aliases/check-dns-alias.sh
+++ offline/custom-dns-record-with-dns-aliases/check-dns-alias.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-export TAS='cf-f30f16d0a030d67be63a'
-bosh -d $TAS ssh diego_cell -c 'dig autoscale.sys.lab.pcfdemo.net'
-bosh -d $TAS ssh clock_global -c 'dig autoscale.sys.lab.pcfdemo.net'
-bosh -d $TAS ssh cloud_controller -c 'dig autoscale.sys.lab.pcfdemo.net'
-bosh -d $TAS ssh cloud_controller_worker -c 'dig autoscale.sys.lab.pcfdemo.net'
+export DS='cf-f30f16d0a030d67be63a'
+bosh -d $DS ssh diego_cell -c 'dig autoscale.sys.lab.pcfdemo.net'
+bosh -d $DS ssh clock_global -c 'dig autoscale.sys.lab.pcfdemo.net'
+bosh -d $DS ssh cloud_controller -c 'dig autoscale.sys.lab.pcfdemo.net'
+bosh -d $DS ssh cloud_controller_worker -c 'dig autoscale.sys.lab.pcfdemo.net'
