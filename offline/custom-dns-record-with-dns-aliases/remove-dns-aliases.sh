3c1a0d474074a53ea46757cf440c6ff24ac10c95
Minseok Kim <myminseok@users.noreply.github.com>
2025-02-28 20:03:26 +0900

    Update remove-dns-aliases.sh

--- offline/custom-dns-record-with-dns-aliases/remove-dns-aliases.sh
+++ offline/custom-dns-record-with-dns-aliases/remove-dns-aliases.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-export TAS='cf-f30f16d0a030d67be63a'
-bosh -d $TAS ssh clock_global -c 'sudo rm -rf /var/vcap/jobs/my-aliases/dns/; sudo /var/vcap/bosh/bin/monit restart bosh-dns'
-bosh -d $TAS ssh diego_cell -c 'sudo rm -rf /var/vcap/jobs/my-aliases/dns/; sudo /var/vcap/bosh/bin/monit restart bosh-dns'
-bosh -d $TAS ssh cloud_controller -c 'sudo rm -rf /var/vcap/jobs/my-aliases/dns/; sudo /var/vcap/bosh/bin/monit restart bosh-dns'
-bosh -d $TAS ssh cloud_controller_worker -c 'sudo rm -rf /var/vcap/jobs/my-aliases/dns/; sudo /var/vcap/bosh/bin/monit restart bosh-dns'
+export DS='cf-f30f16d0a030d67be63a'
+bosh -d $DS ssh clock_global -c 'sudo rm -rf /var/vcap/jobs/my-aliases/dns/; sudo /var/vcap/bosh/bin/monit restart bosh-dns'
+bosh -d $DS ssh diego_cell -c 'sudo rm -rf /var/vcap/jobs/my-aliases/dns/; sudo /var/vcap/bosh/bin/monit restart bosh-dns'
+bosh -d $DS ssh cloud_controller -c 'sudo rm -rf /var/vcap/jobs/my-aliases/dns/; sudo /var/vcap/bosh/bin/monit restart bosh-dns'
+bosh -d $DS ssh cloud_controller_worker -c 'sudo rm -rf /var/vcap/jobs/my-aliases/dns/; sudo /var/vcap/bosh/bin/monit restart bosh-dns'
